9ce7c62f097942f0e030f0cd6a0c58020718dc1b
aryarefman <155049856+aryarefman@users.noreply.github.com>
2025-10-21 10:35:00 +0700

    ulang

--- src/infrastructures/config/prisma.ts
+++
@@ -1,7 +0,0 @@
-// File: src/infrastructures/config/prisma.ts
-import { PrismaClient } from "@prisma/client";
-
-// Export instance tunggal dari Prisma Client
-const prisma = new PrismaClient();
-
-export default prisma;
